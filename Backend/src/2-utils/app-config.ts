8844b2bd79ddbb9891c699b1c7709581c0f33c2e
Michal Shenkar <0583294585m@gmail.com>
2023-06-13 12:16:52 +0300

    frontend - init project

--- Backend/src/2-utils/app-config.ts
+++ Backend/src/2-utils/app-config.ts
@@ -3,7 +3,7 @@ class AppConfig {
     public connectionString = "mongodb://127.0.0.1:27017/gift-shop"
 
     public port = 3001
-    public frontEndUrl = "http://localhost:3000"
+    public frontEndUrl = "http://localhost:4200"
 }
 
 
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,17 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout-area/layout/layout.component';
+import { HeaderComponent } from './components/layout-area/header/header.component';
+import { FooterComponent } from './components/layout-area/footer/footer.component';
+import { MainComponent } from './components/layout-area/main/main.component';
+import { MenuComponent } from './components/layout-area/menu/menu.component';
+import { PageNotFoundComponent } from './components/layout-area/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    MenuComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LayoutComponent]
 })
 export class AppModule { }
